f1ee46b06d30813ae407ced8d2e1ee05c5c929ca
ravindrakaya <kayaravindra118@gmail.com>
2023-12-15 14:45:32 +0000

    solution

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
 
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
@@ -26,7 +27,7 @@ const initializeDbAndServer = async () => {
 
     await db.run(`CREATE TABLE IF NOT EXISTS
              user (
-                id INTEGER PRIMARY KEY ,
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name TEXT,
                 email TEXT,
                 phone INT,
@@ -43,17 +44,74 @@ const initializeDbAndServer = async () => {
 };
 initializeDbAndServer();
 
+// settingUp Nodemailer Configuration
+
+const creatingTestAccount = async () => {
+  const testAccount = await nodemailer.createTestAccount();
+  return {
+    user: testAccount.user,
+    pass: testAccount.pass,
+    smtp: testAccount.smtp,
+  };
+};
+
+const etherealConfig = {
+  service: "ethereal",
+  auth: creatingTestAccount(),
+};
+
+const transporter = nodemailer.createTransport(etherealConfig);
+
 // Signup api
 app.post("/signup", async (req, res) => {
   const { name, email, phone, username, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
+  const selectUserQuery = "SELECT * FROM user WHERE username = ?";
+
+  // send a welcome mail
+  const mailOptions = {
+    from: "www.kayaravindra118@gmail.com",
+    to: email,
+    subject: "Welcome to my app",
+    text: `Hello ${username}, \n\nThank you for registering my app `,
+  };
+
   try {
     await db.run(
       "INSERT INTO user (name,email,phone,username,password) VALUES (?, ?,?, ?,?)",
       [name, email, phone, username, hashedPassword]
     );
     res.send("User registered successfully!");
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    res.send(
+      "User registered successfully! Check your email for a welcome message."
+    );
   } catch (err) {
     res.status(500).send(err.message);
+  }
+});
+
+// login api
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const selectQuery = "SELECT * FROM user WHERE username = ?";
+  try {
+    const dbUser = await db.get(selectQuery, [username]);
+    // console.log(dbUser);
+    if (dbUser === undefined) {
+      res.status(400);
+      res.send("Invalid User");
+    } else {
+      const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+      if (isPasswordMatched === true) {
+        res.send("Login Success!");
+      } else {
+        res.status(400);
+        res.send("Invalid Password");
+      }
+    }
+  } catch (err) {
+    console.log(err.message);
   }
 });
